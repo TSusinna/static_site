4cad56516506174c2fe7f79c8126828a746a98d8
TSusinna <tomas.susinna@outlook.com>
2025-05-18 00:16:40 +0000

    1.2 - Setting up Github pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -5,21 +5,21 @@ from markdown_blocks import markdown_to_html_node
 # This function recursively generates HTML pages from markdown files in the content directory
 # It takes the content directory path, template file path, and destination directory path as arguments
 # It iterates through the files in the content directory, and for each markdown file,
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 # This function generates a page from a markdown file and a template file
 # and saves the generated HTML to the destination path
 # It reads the markdown content, converts it to HTML, and replaces placeholders in the template with the title and content
 # It also creates the destination directory if it doesn't exist
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -35,6 +35,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from textnode import TextNode, TextType
 from copystatic import copy_files_recursive
@@ -7,14 +8,20 @@ from gencontent import generate_pages_recursive
 
 # Defines the paths for the static and public directories, content directory, and template file
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
-# This function calls copy_files_recursive to clean and copy files from the static directory to the public directory
-# and then calls generate_page to generate the HTML page from the markdown file
-# It also prints the progress of the operations
+# This function generates a static site by copying static files and generating HTML pages from markdown files
+# It takes the base path as an argument, which is used to set the base URL for the generated pages
+# It first deletes the public directory if it exists, then copies the static files to the public directory
+# It then generates HTML pages from the markdown files in the content directory using the specified template
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -23,7 +30,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
     print("Done!")
 
 main()
